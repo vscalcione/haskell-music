88e547ec6fa13b8bd17f811d9c160b8176698204
Vincenzo Scalcione <vincenzo99.scalcione@gmail.com>
2020-07-14 15:30:35 +0200

    Added start.sh for automate script's starting

--- Main.hs
+++ Main.hs
@@ -2,6 +2,12 @@ import qualified Data.ByteString.Lazy as B
 import qualified Data.ByteString.Builder as B
 import Data.Foldable
 
+volume :: Float
+volume = 0.5
+
 wave :: [Float]
-wave = map sin $ map  (* step) [0.0 .. 48000]
-    where step = 0.01
+wave = map (* volume) $ map sin $ map  (* step) [0.0 .. 48000]
+    where step = 0.05
+
+save :: IO()
+save = B.writeFile "output.bin" $ B.toLazyByteString $ fold $ map B.floatLE wave
---
+++ start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+ffplay -f f32le -ar 48000 output.bin &
