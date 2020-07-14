4df3826acf528176328b5f0b2c3583b33c7b6813
Vincenzo Scalcione <vincenzo99.scalcione@gmail.com>
2020-07-14 16:35:32 +0200

    Fixing code with print sine curve in output.bin

--- Main.hs
+++ Main.hs
@@ -1,13 +1,28 @@
 import qualified Data.ByteString.Lazy as B
 import qualified Data.ByteString.Builder as B
 import Data.Foldable
+import System.Process
+import Text.Printf
+
+outputFilePath :: FilePath
+outputFilePath = "output.bin"
+
 
 volume :: Float
 volume = 0.5
 
-wave :: [Float]
-wave = map (* volume) $ map sin $ map  (* step) [0.0 .. 48000]
-    where step = 0.05
+sampleRate :: Float
+sampleRate = 48000
 
-save :: IO()
-save = B.writeFile "output.bin" $ B.toLazyByteString $ fold $ map B.floatLE wave
+wave :: [Float]
+wave = map (* volume) $ map sin $ map  (* step) [0.0 .. sampleRate]
+    where step = 0.02
+
+save :: FilePath -> IO()
+save filePath = B.writeFile "output.bin" $ B.toLazyByteString $ fold $ map B.floatLE wave
+
+play :: IO()
+play = do
+    save outputFilePath
+    _ <- runCommand $ printf "ffplay -showmode 1 -f f32le -ar %f %s" sampleRate outputFilePath
+    return ()
