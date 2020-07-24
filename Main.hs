a75ddbaeceea675d765f28bdc52e1d6205497b0c
Vincenzo Scalcione <vincenzo99.scalcione@gmail.com>
2020-07-24 19:28:20 +0200

    Refactoring code

--- Main.hs
+++ Main.hs
@@ -1,12 +1,16 @@
-import qualified Data.ByteString.Lazy as B
-import qualified Data.ByteString.Builder as B
+import qualified Data.ByteString.Lazy as B 
+import qualified Data.ByteString.Builder as B 
 import Data.Foldable
 import System.Process
 import Text.Printf
+import Data.List
 
+type Pulse = Float
 type Seconds = Float
 type Samples = Float
 type Hz = Float
+type Semitones = Float
+type Beats = Float
 
 outputFilePath :: FilePath
 outputFilePath = "output.bin"
@@ -15,21 +19,89 @@ volume :: Float
 volume = 0.5
 
 sampleRate :: Samples
-sampleRate = 48000
+sampleRate = 48000.0
 
-wave :: [Float]
-wave = map (* volume) $ map sin $ map  (* step) [0.0 .. sampleRate * duration]
-    where step = 0.02
-        duration :: Seconds
-        duration = 0.5
-        hz :: Float
-        hz = 440.0
+pitchStandard :: Hz
+pitchStandard = 440.0
 
-save :: FilePath -> IO()
-save filePath = B.writeFile "output.bin" $ B.toLazyByteString $ fold $ map B.floatLE wave
+bpm :: Beats
+bpm = 120.0
 
-play :: IO()
+beatDuration :: Seconds
+beatDuration = 60.0 / bpm
+
+f :: Semitones -> Hz
+f n = pitchStandard * (2 ** (1.0 / 12.0)) ** n
+
+note :: Semitones -> Beats -> [Pulse]
+note n beats = freq (f n) (beats * beatDuration)
+
+freq :: Hz -> Seconds -> [Pulse]
+freq hz duration = 
+  map (* volume) $ zipWith3 (\x y z -> x * y * z) release attack output  
+  where
+    step = (hz * 2 * pi) / sampleRate
+
+    attack :: [Pulse]
+    attack = map (min 1.0) [0.0, 0.001 ..]
+
+    release :: [Pulse]
+    release = reverse $ take (length output) attack
+
+    output :: [Pulse]
+    output = map sin $ map (* step) [0.0 .. sampleRate * duration]
+
+wave :: [Pulse]
+wave = concat [ 
+  note 0 0.25, 
+  note 0 0.25,
+  note 0 0.25,
+  note 0 0.25,
+  note 0 0.5,
+  note 0 0.25,
+  note 0 0.25,
+  note 0 0.25,
+  note 0 0.25,
+  note 0 0.25,
+  note 0 0.5,
+  note 0 0.25,
+  note 0 0.25,
+  note 0 0.25,
+  note 0 0.25,
+  note 0 0.25,
+  note 0 0.25,
+  note 0 0.5,
+  note 5 0.25,
+  note 5 0.25,
+  note 5 0.25,
+  note 5 0.25,
+  note 5 0.25,
+  note 5 0.25,
+  note 5 0.5,
+  note 3 0.25,
+  note 3 0.25,
+  note 3 0.25,
+  note 3 0.25,
+  note 3 0.25,
+  note 3 0.25,
+  note 3 0.5,
+  note (-2) 0.25,
+  note 0 0.25,
+  note 0 0.25,
+  note 0 0.25,
+  note 0 0.25,
+  note 0 0.25,
+  note 0 0.25,
+  note 0 0.5
+  ]
+  where
+    duration = 0.5
+
+save :: FilePath -> IO ()
+save filePath = B.writeFile filePath $ B.toLazyByteString $ fold $ map B.floatLE wave
+
+play :: IO ()
 play = do
-    save outputFilePath
-    _ <- runCommand $ printf "ffplay -showmode 1 -f f32le -ar %f %s" sampleRate outputFilePath
-    return ()
+  save outputFilePath
+  _ <- runCommand $ printf "ffplay -showmode 1 -f f32le -ar %f %s" sampleRate outputFilePath
+  return ()
